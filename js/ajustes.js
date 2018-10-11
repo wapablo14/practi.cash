97c6083629545fa1f747987b968b63ee081249ec
cf015 <cap.freire@yavirac.edu.ec>
2018-10-11 17:43:25 -0500

    Funcionalidad practicash

--- js/ajustes.js
+++ js/ajustes.js
@@ -1,34 +1,73 @@
 $(document).ready(function(){
-
-jQuery.validator.addMethod("nombre", function(value, element) {
+	
+	jQuery.validator.addMethod("name", function(value, element) {
         return this.optional(element) || /^[a-záéóóúàèìòùäëïöüñ\s]+$/i.test(value);
         });  
-
+	
     /* Validar el campo nombre */
-        jQuery.validator.addMethod("apellido", function(value, element) {
+        jQuery.validator.addMethod("fullname", function(value, element) {
         return this.optional(element) || /^[a-záéóóúàèìòùäëïöüñ\s]+$/i.test(value);
         });     
-
     /* Validar el formulario */
-    $("#btnActualizar").on("click", function(){
+    $("#btnActualizar").click(function(){
         var formulario = $("#formulario").validate
-         ({
-             rules: /* Accedemos a los campos.*/
-             {
-               
-               nombre: {required: true,  nombre: true },
-               apellido: {required: true, apellido: true}
-               
-              
-             },
-             messages:
-             {
-                
-                nombre : {required: 'Este campo es obligatorio', nombre:'Ingrese su Nombre'},
-                apellido : {required: 'Este campo es obligatorio', apellido:'Ingrese su Apellido'}
-                
-                
-             },
+		({
+			rules: /* Accedemos a los campos.*/
+			{
+				name: {required: true,  name: true },
+				fullname: {required: true, fullname: true}
+			},
+			messages:
+			{
+				name : {required: 'Este campo es obligatorio', name:'Ingrese su Nombre'},
+				fullname : {required: 'Este campo es obligatorio', fullname:'Ingrese su Apellido'}
+			},
+			submitHandler: function(form){
+				var numero=$("#numero").val();
+				var name=$("#name").val();
+				var fullname=$("#fullname").val();
+				var password=$("#password").val();
+				var dataString="numero="+numero+"&name="+name+"&fullname="+fullname+"&password="+password+"&update=";
+				if($.trim(numero).length>0)
+				{
+					$.ajax({
+					type: "POST",
+					url:"https://paulina96madrid.000webhostapp.com/datosPrueba/update.php",
+					data: dataString,
+					crossDomain: true,
+					cache: false,
+					beforeSend: function(){ $("#insert").val('Connecting...');},
+					success: function(data){
+					if(data=="success")
+					{
+						alert("Registro actualizado.");
+						window.location.href = "usuario.html";
+					}
+					else if(data=="error")
+					{
+						alert("Error de registro.");
+					}
+					}
+					});
+				}
+				return false;
+			}
         });
-    });          
+    });  
+	//Valores base de datos en inputs.	
+	var url="https://paulina96madrid.000webhostapp.com/datosPrueba/datos.php";
+		$.getJSON(url,function(result){
+			console.log(result);
+			$.each(result, function(i, field){
+				var id=field.id;
+				var id_num=field.id_num;
+				var name=field.name;
+				var fullname=field.fullname;
+				var password=field.password;
+				$("#numero").val(id_num);
+				$("#name").val(name);
+				$("#fullname").val(fullname);
+				$("#password").val(password);
+			});
+		});
 });
--- js/index.js
+++ js/index.js
@@ -21,26 +21,75 @@ $(document).ready(function(){
 			unhighlight: function(element){
 			},
 			/*Validacion correcta metodo.*/
-			submitHandler: function(form){
+			submitHandler: function(form){	
 				
-				//window.location.href = "Paginas/index.html";
-				
-				//Cargar librerias de Cordova.
-				document.addEventListener("deviceready", onDeviceReady, false);
-				//Funcion ready. 
-				function onDeviceReady() {
+				//Ajax comprobacion inputs.
+				var numero=$("#numero").val();
+				var dataString="numero="+numero+"&login=";
+				if($.trim(numero).length>0){
+				$.ajax({
+					type: "POST",
+					url: "https://paulina96madrid.000webhostapp.com/datosPrueba/loginPrueba.php",
+					crossDomain: true, 
+					cache: false,
+					data: dataString,
+					success: function(data){
+					if(data == "success"){    
+						localStorage.loginstatus = "true";
+						window.location.href = "Paginas/usuario.html";
+					}
+					else if(data == "error"){
+						alert('No registrado.');
+						alert('Registro.');
+						//Ajax envio inputs.
+						var numero=$("#numero").val();
+						var dataString="numero="+numero+"&insert=";
+						$.ajax({
+							type: "POST",
+							url:"https://paulina96madrid.000webhostapp.com/datosPrueba/loginPrueba.php",
+							data: dataString,
+							crossDomain: true,
+							cache: false,
+							beforeSend: function(){ $("#insert").val('Connecting...');},
+							success: function(data){
+							if(data=="success")
+							{
+								alert("Registro creado.");
+								window.location.href = "Paginas/registro.html";
+							}
+							else if(data=="error")
+							{
+								alert("Error de registro.");
+							}
+							else if(data == "exist")
+							{
+								alert("Registro ya existe.");
+							}
+							}
+						});              
+					}
+					}
+				});
 				}
-				//Funcion alerta.
-				function alertDismissed() {
-				window.location.href = "Paginas/menuIndex.html";
-				}	
-				//Alerta en cordova.
-				navigator.notification.alert(
-					'No tienes cuenta.',  // message
-					alertDismissed,         // callback
-					'PractiCash',            // title
-					'Registrar'                 // buttonName
-				);
+			return false;
+					
+					//window.location.href = "Paginas/index.html";			
+					/*Cargar librerias de Cordova.
+					document.addEventListener("deviceready", onDeviceReady, false);
+					Funcion ready. 
+					function onDeviceReady() {
+					}
+					Funcion alerta.
+					function alertDismissed() {
+					window.location.href = "Paginas/index.html";
+					}	
+					Alerta en cordova.
+					navigator.notification.alert(
+						'No tienes cuenta.',  // message
+						alertDismissed,         // callback
+						'PractiCash',            // title
+						'Registrar'                 // buttonName
+					);*/
 			}	
 		});	
 	});
